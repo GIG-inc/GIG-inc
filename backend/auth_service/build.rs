862f303c40b8716829dd0a375abe4cfa88b336c3
Mark <markmoreka@gmail.com>
2026-02-02 15:45:33 +0300

    fix build in auth

--- backend/auth_service/build.rs
+++ backend/auth_service/build.rs
@@ -1,7 +1,15 @@
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let protoc_path = protoc_bin_vendored::protoc_bin_path()?;
+
+    // Tell Cargo to set PROTOC for this build (safe, no unsafe block)
+    println!("cargo:rustc-env=PROTOC={}", protoc_path.display());
+
     tonic_build::configure()
         .build_server(true)
         .build_client(false)
-        .compile_protos(&["../proto/auth.proto"], &["../proto"])
-        .unwrap();
+        .compile_protos(&["../proto/auth.proto"], &["../proto"])?;
+
+    println!("cargo:rerun-if-changed=../proto");
+
+    Ok(())
 }
